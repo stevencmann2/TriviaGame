409112d85477aec3e1d2a17990964df0ccb0f696
Steven Mann <stevencmann2@gmail.com>
2019-10-18 12:46:31 -0500

    commented the trivia questions and answers in app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,50 @@
 console.log("hello world!");
+
+
+
+
+
+
+
+
+
+
+
+
+// Questions and answers for Trivia//
+
+/*    
+Q1: In Don Mclean's "American Pie," he references the day the music died. Who's death is he referencing?
+A1: Buddy Holly
+
+Q2: Kirk Cobain, Jimi Hendrix, Jim Morrison and Janis Jolin all died at what, now notorious, age?
+A2: 27 
+
+Q3: What color was the "bad acid" at Woodstock?
+A3: Brown
+
+Q4: What was the name of the road the Beatles crossed on their famous album dubbed from the same name?
+A4: Abbey Road
+
+Q5:Jerry Garica was the lead guitarist for what iconoclast American Rock and Roll Group?
+A5; Grateful Dead
+
+Q6: What is the largest grossing touring in Rock and Roll history, grossing nearly $740 million?
+A6: U2 - 360 Tour
+
+Q7: Who was the lead guitarist of the band Cream?
+A7: Eric Clapton
+
+Q8: Which song was the first #1 hit of the 2000's & ranked as the 2nd most succesful song of all-time by Billboard Magazine?
+A8: "Smooth" - Santana ft. Rob Thomas
+
+Q9; Which drummer was found dead after consuming the equivalent of 40 vodka shots?
+A9: John Bonham
+
+Q10;Who recorded the guitar solo for Michael Jackson's hit song "Beat it"?
+A10; Eddie Van Halen
+
+
+
+
+*/ 
