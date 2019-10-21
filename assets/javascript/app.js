eadeb871656a92d67ac6d53aaf4ecdb3ab7c3fed
Steven Mann <stevencmann2@gmail.com>
2019-10-21 14:05:21 -0500

    debugging start & click functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@ const question = ["In Don Mclean's 'American Pie,' he references the day the mus
         "Which song was the first #1 hit of the 2000's, the last #1 hit of 1999, & ranked as the 2nd most succesful song of all-time by Billboard Magazine?", "Which drummer was found dead after consuming the equivalent of 40 vodka shots?", "Who is credited with recording the guitar solo for Michael Jackson's hit song 'Beat it'?",
     ];
 const correct = ["Buddy Holly", "27", "Brown", "Abbey Rd.", "The Grateful Dead", "U2 - 360 Tour (2009)", "Eric Clapton", "'Smooth' - Santana ft. Rob Thomas", "John Bonham", "Eddie Van Halen", ];
-const ChoiceA = ["Martin Luther King Jr.", "26", "Yellow", "Main St.", "The Kinks", "U2 - 360 Tour (2009)", "Jimmy Paige", "'Higher' - Creed", "Keith Moon", "Jimi Hendrix", ];
+const choiceA = ["Martin Luther King Jr.", "26", "Yellow", "Main St.", "The Kinks", "U2 - 360 Tour (2009)", "Jimmy Paige", "'Higher' - Creed", "Keith Moon", "Jimi Hendrix", ];
 const choiceB = ["Buddy Holly", "25", "Blue", "Noname St.", "The Grateful Dead", "Rolling Stones - Bigger Band Tour (2005)", "Geroge Harrison", "'Smooth' - Santana ft. Rob Thomas", "John Bonham", "Angus Young", ];
 const choiceC = ["John F. Kennedy", "27", "Red", "Abbey Rd.", "The Yardbirds", "Pink Floyd -The Wall Tour (1980)", "Keith Richards", "'Kryptonite' - 3 Doors Down", "Earl Palmer", "Jeff Beck", ];
 const choiceD = ["Billie Holiday", "28", "Brown", "Penny Ln.", "Velvet Underground", "Taylor Swift - 1989 Tour (2015)", "Eric Clapton", "'With Arms Wide Open' - Creed", "Ginger Baker", "Eddie Van Halen", ];
@@ -25,28 +25,28 @@ let questionNumber = 0;
 
 
 //By clicking the start button, we will hide the contents of the start screen completely from the user//
-const StartBtn = $(".start").click(function (event) {
-    playingGame = true;
+const startBtn = $(".start").click(showQuestion) {
+    playingGame === true;
     $(".startscreen").hide();
     $(".card").show(); //this should show the first question function?
-    console.log(playingGame);
-});
+};
+
+
 
 //show question and choices function?
 function showQuestion (){
-    $(".question.text").html(question);
-    $(".ChoiceA-text").html(choiceA);
-    $(".ChoiceB-text").html(choiceB);
-    $(".ChoiceC-text").html(choiceC);
-    $(".ChoiceD-text").html(choiceD);
-
+    $(".question.text").text(question[0]);
+    $(".ChoiceA-text").html(choiceA[0]);
+    $(".ChoiceB-text").html(choiceB[0]);
+    $(".ChoiceC-text").html(choiceC[0]);
+    $(".ChoiceD-text").html(choiceD[0]);
 };
 //start game function after start click?
-function gameStart(){
+/*function gameStart(){
  if playingGame = true {
      
  }
-};
+}; */
 
 // need to display the first question and choices for the user//
 //but need to make a function for that//
