68116c2cf929c6b983ac16655daba0b0866d4eda
Steven Mann <stevencmann2@gmail.com>
2019-10-21 19:08:44 -0500

    Debugging object/properties and print functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,54 +1,62 @@
 // Javascript for Rock and Roll Trivia
 //$(".start") is the start button at the start of the game
 
-//The Triva game contains arrays of all the questions, choices and correct answers to the trivia components//
+//The triva game contains arrays of all the questions, choices and correct answers to the trivia components//
 
-const Trivia = {
-    question: "In Don Mclean's 'American Pie,' he references the day the music died. Who's death is he referencing?", "Kirk Cobain, Jimi Hendrix, Jim Morrison and Janis Joplin all died at what, now notorious, age?",
-    choice: ["Buddy Holly","Martin Luther King Jr.","Buddy Holly","John F. Kennedy",]
+const trivia = 
+[{
+
+    question: "In Don Mclean's 'American Pie,' he references the day the music died. Who's death is he referencing?", 
+    choice: ["Buddy Holly","Martin Luther King Jr.","Buddy Holly","John F. Kennedy"],
     correct: "Billie Holiday",
+},
 
-    question:"What color was the 'bad acid' at Woodstock?",
-    choice:["26","27","28","25",],
+{    
+    question:"Jimi Hendrix, Janis Joplin and Kurt Cobain all died at what, now notroious, age?",
+    choice:["26","27","28","25"],
     correct: "27",
-
+},
+{
     question:"What was the name of the road the Beatles crossed on their famous album dubbed from the same name?",
-    choice:["Noname St.","Penny Ln.","Abbey Rd.","Main St.",],
+    choice:["Noname St.","Penny Ln.","Abbey Rd.","Main St."],
     correct:"Abbey Rd.",
-
+},
+{
     question:"Jerry Garica was the lead guitarist for which iconoclast American Rock and Roll Group?",
-    choice:["The Rolling Stones","The Grateful Dead","The Kinks", "The Yardbirds",],
-    correct:"The Grateful Dead"
-
+    choice:["The Rolling Stones","The Grateful Dead","The Kinks", "The Yardbirds"],
+    correct:"The Grateful Dead",
+},
+{
     question:"What is the largest grossing tour in Rock and Roll history, grossing nearly $740 million?",
-    choice: ["U2 - 360 Tour (2009)","Rolling Stones - Bigger Band Tour (2005)","Pink Floyd -The Wall Tour (1980)","Taylor Swift - 1989 Tour (2015)",],
+    choice: ["U2 - 360 Tour (2009)","Rolling Stones - Bigger Band Tour (2005)","Pink Floyd -The Wall Tour (1980)","Taylor Swift - 1989 Tour (2015)"],
     correct:"U2 - 360 Tour (2009)",
+},
+{
 
     question:"Although not credited on the album, which guitarist actually recorded the solo for the Beatles 'While My Guitar Gently Weeps', which appeared on the Beatles 'White Album'?",
     choice:["Eric Clapton", "Jimmy Paige", "Geroge Harrison", "Keith Richards"],
     correct:"Eric Clapton",
-
+},
+{
     question:"Which song was the first #1 hit of the 2000's, the last #1 hit of 1999, & ranked as the 2nd most succesful song of all-time by Billboard Magazine?",
     choice:["'With Arms Wide Open' - Creed","'Smooth' - Santana ft. Rob Thomas","'Higher' - Creed","'Kryptonite' - 3 Doors Down",],
     correct:"'Smooth' - Santana ft. Rob Thomas",
+},
+{
 
     question: "Which drummer was found dead after consuming the equivalent of 40 vodka shots?",
-    choice:[ "Keith Moon", "Earl Palmer", "John Bonham","Ginger Baker",]
+    choice:[ "Keith Moon", "Earl Palmer", "John Bonham","Ginger Baker"],
     correct:"John Bonham",
-
+},
+{
     question: "Who is credited with recording the guitar solo for Michael Jackson's hit song 'Beat it'?",
-    choice:["Jeff Beck", "Angus Young", "Jimi Hendrix", "Eddie Van Halen",]
+    choice:["Jeff Beck", "Angus Young", "Jimi Hendrix", "Eddie Van Halen"],
     correct: "Eddie Van Halen",
-         
+},    
           
-};
+];
 
 
-//how to set correct Array to a value of true//
-for (i=0; i< correct.length;i++) {
-    correct = true;
-};
-console.log(correct);
 
 //will eventually make correct choice = Choice[i;thisclick]//
 // variables to be declared as of now//
@@ -73,11 +81,8 @@ function showQuestion (){
     playingGame === true;
     $(".startscreen").hide();
     $(".card").show();
-    $(".question-text").html(question[questionNumber]);
-    $(".choice").html(choiceA[questionNumber]);
-    $(".choice").html(choiceB[questionNumber]);
-    $(".choice").html(choiceC[questionNumber]);
-    $(".choice").html(choiceD[questionNumber]);
+    $(".question-text").html(trivia[questionNumber].question);
+    $(".choice").html(trivia[questionNumber].choice);
 };
 
 //next question function?
