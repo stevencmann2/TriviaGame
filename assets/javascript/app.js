a52378f00d33367ecfd9d75000425ddafc38b8c7
Steven Mann <stevencmann2@gmail.com>
2019-10-21 19:36:17 -0500

    concole.log results to see if correct

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,57 +3,56 @@
 
 //The triva game contains arrays of all the questions, choices and correct answers to the trivia components//
 
-const trivia = 
-[{
+const trivia = [{
 
-    question: "In Don Mclean's 'American Pie,' he references the day the music died. Who's death is he referencing?", 
-    choice: ["Billie Holiday","Martin Luther King Jr.","Buddy Holly","John F. Kennedy"],
-    correct: "Buddy Holly",
-},
+        question: "In Don Mclean's 'American Pie,' he references the day the music died. Who's death is he referencing?",
+        choice: ["Billie Holiday", "Martin Luther King Jr.", "Buddy Holly", "John F. Kennedy"],
+        correct: "Buddy Holly",
+    },
 
-{    
-    question:"Jimi Hendrix, Janis Joplin and Kurt Cobain all died at what, now notroious, age?",
-    choice:["26","27","28","25"],
-    correct: "27",
-},
-{
-    question:"What was the name of the road the Beatles crossed on their famous album dubbed from the same name?",
-    choice:["Noname St.","Penny Ln.","Abbey Rd.","Main St."],
-    correct:"Abbey Rd.",
-},
-{
-    question:"Jerry Garica was the lead guitarist for which iconoclast American Rock and Roll Group?",
-    choice:["The Rolling Stones","The Grateful Dead","The Kinks", "The Yardbirds"],
-    correct:"The Grateful Dead",
-},
-{
-    question:"What is the largest grossing tour in Rock and Roll history, grossing nearly $740 million?",
-    choice: ["U2 - 360 Tour (2009)","Rolling Stones - Bigger Band Tour (2005)","Pink Floyd -The Wall Tour (1980)","Taylor Swift - 1989 Tour (2015)"],
-    correct:"U2 - 360 Tour (2009)",
-},
-{
+    {
+        question: "Jimi Hendrix, Janis Joplin and Kurt Cobain all died at what, now notroious, age?",
+        choice: ["26", "27", "28", "25"],
+        correct: "27",
+    },
+    {
+        question: "What was the name of the road the Beatles crossed on their famous album dubbed from the same name?",
+        choice: ["Noname St.", "Penny Ln.", "Abbey Rd.", "Main St."],
+        correct: "Abbey Rd.",
+    },
+    {
+        question: "Jerry Garica was the lead guitarist for which iconoclast American Rock and Roll Group?",
+        choice: ["The Rolling Stones", "The Grateful Dead", "The Kinks", "The Yardbirds"],
+        correct: "The Grateful Dead",
+    },
+    {
+        question: "What is the largest grossing tour in Rock and Roll history, grossing nearly $740 million?",
+        choice: ["U2 - 360 Tour (2009)", "Rolling Stones - Bigger Band Tour (2005)", "Pink Floyd -The Wall Tour (1980)", "Taylor Swift - 1989 Tour (2015)"],
+        correct: "U2 - 360 Tour (2009)",
+    },
+    {
 
-    question:"Although not credited on the album, which guitarist actually recorded the solo for the Beatles 'While My Guitar Gently Weeps', which appeared on the Beatles 'White Album'?",
-    choice:["Eric Clapton", "Jimmy Paige", "Geroge Harrison", "Keith Richards"],
-    correct:"Eric Clapton",
-},
-{
-    question:"Which song was the first #1 hit of the 2000's, the last #1 hit of 1999, & ranked as the 2nd most succesful song of all-time by Billboard Magazine?",
-    choice:["'With Arms Wide Open' - Creed","'Smooth' - Santana ft. Rob Thomas","'Higher' - Creed","'Kryptonite' - 3 Doors Down",],
-    correct:"'Smooth' - Santana ft. Rob Thomas",
-},
-{
+        question: "Although not credited on the album, which guitarist actually recorded the solo for the Beatles 'While My Guitar Gently Weeps', which appeared on the Beatles 'White Album'?",
+        choice: ["Eric Clapton", "Jimmy Paige", "Geroge Harrison", "Keith Richards"],
+        correct: "Eric Clapton",
+    },
+    {
+        question: "Which song was the first #1 hit of the 2000's, the last #1 hit of 1999, & ranked as the 2nd most succesful song of all-time by Billboard Magazine?",
+        choice: ["'With Arms Wide Open' - Creed", "'Smooth' - Santana ft. Rob Thomas", "'Higher' - Creed", "'Kryptonite' - 3 Doors Down", ],
+        correct: "'Smooth' - Santana ft. Rob Thomas",
+    },
+    {
 
-    question: "Which drummer was found dead after consuming the equivalent of 40 vodka shots?",
-    choice:[ "Keith Moon", "Earl Palmer", "John Bonham","Ginger Baker"],
-    correct:"John Bonham",
-},
-{
-    question: "Who is credited with recording the guitar solo for Michael Jackson's hit song 'Beat it'?",
-    choice:["Jeff Beck", "Angus Young", "Jimi Hendrix", "Eddie Van Halen"],
-    correct: "Eddie Van Halen",
-},    
-          
+        question: "Which drummer was found dead after consuming the equivalent of 40 vodka shots?",
+        choice: ["Keith Moon", "Earl Palmer", "John Bonham", "Ginger Baker"],
+        correct: "John Bonham",
+    },
+    {
+        question: "Who is credited with recording the guitar solo for Michael Jackson's hit song 'Beat it'?",
+        choice: ["Jeff Beck", "Angus Young", "Jimi Hendrix", "Eddie Van Halen"],
+        correct: "Eddie Van Halen",
+    },
+
 ];
 
 
@@ -73,11 +72,11 @@ let questionNumber = 0;
 
 //By clicking the start button, we will hide the contents of the start screen completely from the user//
 //Use jQuery to run "showQuestion" when we click the "start" button.
-$(".start").click(showQuestion) 
+$(".start").click(showQuestion)
 //this should show the first question function?
 
 //show question and choices function?
-function showQuestion (){
+function showQuestion() {
     playingGame === true;
     $(".startscreen").hide();
     $(".card").show();
@@ -89,7 +88,7 @@ function showQuestion (){
 };
 
 //next question function?
-function nextQuestion () {
+function nextQuestion() {
     questionNumber++;
 
 };
@@ -98,11 +97,19 @@ function nextQuestion () {
 $(".choice").click(userResponse);
 
 //user answer fucntion? use this to determine value click and compare with correct array//
-function userResponse () {
+function userResponse() {
     const clicked = $(this);
     console.log(clicked.text());
-    if (clicked.text() === trivia[questionNumber].correct)
-    console.log("they match");
+    if (clicked.text() === trivia[questionNumber].correct){
+        console.log("they match");
+    correctAnswers++;
+    }
+    else{
+        wrongAnswers++;
+    }
+console.log(wrongAnswers);
+console.log(correctAnswers);
+    
     // need to put a comparative statement of button text and correct[questionNumber]//
 };
 console.log(correctAnswers);
